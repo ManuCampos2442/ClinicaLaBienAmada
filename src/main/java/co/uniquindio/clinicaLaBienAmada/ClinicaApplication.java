a78fe2de98100c001db6736d575138b0c1b9a0f2
MarianaU <mariana.urreav@uqvirtual.edu.co>
2023-09-04 19:26:48 -0500

    Saludo

--- src/main/java/co/uniquindio/clinicaLaBienAmada/ClinicaApplication.java
+++ src/main/java/co/uniquindio/clinicaLaBienAmada/ClinicaApplication.java
@@ -3,9 +3,16 @@ package co.uniquindio.clinicaLaBienAmada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ClinicaApplication {
+
+
     public static void main(String[] args) {
+
+        System.out.println("Buenasss");
+
         SpringApplication.run(ClinicaApplication.class, args);
     }
+
 }
