d2e18247d941d7270f73ce2882deb87d4633ae61
Santiagohs30 <Shernandezs@uqvirtual.edu.co>
2023-09-08 21:07:56 -0500

    Modifiqué Horario, Especialidad, Médico y Día Libre

--- src/main/java/co/uniquindio/clinicaLaBienAmada/model/DiaLibre.java
+++ src/main/java/co/uniquindio/clinicaLaBienAmada/model/DiaLibre.java
@@ -1,29 +1,32 @@
 package co.uniquindio.clinicaLaBienAmada.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.CollectionIdJdbcTypeCode;
 
 import java.io.Serializable;
+
 @Entity
 @Getter
 @Setter
 @ToString
-
-public class Medico implements Serializable {
+public class DiaLibre implements Serializable {
 
     @Id
 
-    //Pk
+    //PK
 
     private String codigo;
+    
+    private String dia;
 
     //FK
-    private String codigoEspecializacion;
 
-    public Medico() {}
+    private String codigoMedico;
+
+    public DiaLibre() {}
 
 }
