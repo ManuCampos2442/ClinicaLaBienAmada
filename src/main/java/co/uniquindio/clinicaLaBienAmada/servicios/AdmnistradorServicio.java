624db141900942f925f8022208632d98b6beacfc
MarianaU <mariana.urreav@uqvirtual.edu.co>
2023-09-15 20:21:55 -0500

    Actualizacion

--- src/main/java/co/uniquindio/clinicaLaBienAmada/servicios/AdmnistradorServicio.java
+++ src/main/java/co/uniquindio/clinicaLaBienAmada/servicios/AdmnistradorServicio.java
@@ -1,4 +1,23 @@
 package co.uniquindio.clinicaLaBienAmada.servicios;
 
 public interface AdmnistradorServicio {
+
+    void crearMedico();
+
+    void actualizarMedico();
+
+    void eliminarMedico();
+
+    void listarMedicos();
+
+    void obtenerMedico();
+
+    void listarPQRS();
+
+    void responderPQRS();
+
+    void verDetallePQRS();
+
+    void listarCitas();
+
 }
